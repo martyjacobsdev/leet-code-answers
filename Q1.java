17dbb4d4b158b55d386d2a9df8af8af6b7d6dd88
marty <martin.philip.jacobs@gmail.com>
2019-03-12 13:22:39 +1000

    q1

--- Q1.java
+++ Q1.java
@@ -1,5 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
+        int[] results = new int[2];
+        
+        for(int i=0; i < nums.length; i++) {
+            
+            for(int k=i+1; k < nums.length; k++) {
+                
+                if((nums[i] + nums[k]) == target) {
+                    
+                    results[0] = i;
+                    results[1] = k;
+                    break;
+                }
+            }
+            
+        }
+        return results;
     }
 }
